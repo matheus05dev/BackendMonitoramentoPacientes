8ad33d61ecabf81fe103057b4e7dee799ad2927c
Matheus Nunes da Silva <matheus05.nunes81@gmail.com>
2025-06-12 13:09:01 -0300

    Atualização do modelo Pessoa e configurações do projeto

--- src/main/java/com/springwalker/back/model/Pessoa.java
+++ src/main/java/com/springwalker/back/model/Pessoa.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -39,7 +40,7 @@ public class Pessoa {
     @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "data nascimento")
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "pessoa id")
