54d52656217d041db916b0a2e7343b54c454f3bc
Matheus Nunes da Silva <matheus05.nunes81@gmail.com>
2025-09-13 21:08:17 -0300

    commit da documentação do código(swagger)

---
+++ src/main/java/com/springwalker/back/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.springwalker.back.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Monitoramento de Pacientes")
+                        .version("1.0")
+                        .description(
+                                "API para gerenciamento de pacientes, atendimentos, funcionários e quartos em um sistema de monitoramento hospitalar")
+                        .contact(new Contact()
+                                .name("Springwalker")
+                                .email("support@springwalker.com")));
+    }
+}
